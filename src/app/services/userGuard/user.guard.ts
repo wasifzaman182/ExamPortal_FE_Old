bdb96dcea08af119162756a25c556b2caa5599ab
Wasif Zaman <wasifzaman94@gmail.com>
2021-07-03 11:55:23 +0500

    login work
    facing an issue when to login admin (need to click two times on login button) normal user is working fine

--- src/app/services/userGuard/user.guard.ts
+++ src/app/services/userGuard/user.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginService } from '../login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
+
+  constructor(private login: LoginService, private router: Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+      //making guard on admin dashboard
+      if(this.login.isLoggedIn() && this.login.getUserRole() == 'NORMAL'){
+
+        return true;
+      }
+      this.router.navigate(['/login']);
+      return false;
   }
   
 }
